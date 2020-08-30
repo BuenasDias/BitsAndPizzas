45fd0d8d3c47e7bd9dd9c0e661f163aafef99f6b
Максим Толстобров <tmaksim1989@gmail.com>
2020-08-30 11:29:57 +0300

    Second commit

--- app/src/main/java/com/tolstobrov/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/tolstobrov/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,10 +1,16 @@
 package com.tolstobrov.bitsandpizzas;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 class CaptionedImagesAdapter extends RecyclerView.Adapter<CaptionedImagesAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
 
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
     }
 }
